177977786c0a5590d81f0d5a64a074d1ccb7e70f
Kevin Schumacher <KevinSchumacher@t-online.de>
2026-03-02 22:09:41 +0100

    feat:added assets, createt arry myimgs

---
+++ fotogram.js
@@ -0,0 +1,16 @@
+const myimgs = [
+
+    'assets/imgdaniil_kondrashin-cat-9839788_1920.jpg',
+    'assets/img/jhenning-aster-9922917_1920.jpg',
+    'assets/img/kingsinnphotography-bearded-dragon-9821324_1920.jpg',
+    'assets/img/kranich17-book-8664639_1920.jpg',
+    'assets/img/martinophuc-succulents-9949534_1920.jpg',
+    'assets/img/miezekieze-cat-9643209_1920.jpg',
+    'assets/img/nick4fun-owl-10077647_1920.jpg',
+    'assets/img/nordseher-castle-9198810_1920.jpg',
+    'assets/img/peterperhac-squirrel-10123754_1920.jpg',
+    'assets/img/ralf1403-chess-9549682_1920.jpg',
+    'assets/img/ralf1403-chess-9549682_1920.jpg',
+    'assets/img/wolfgang_hasselmann-sunset-10128347_1920.jpg',
+    'assets/img/zlapane_chwile-tenerife-10088999_1920.jpg',
+]
